baef5150c4987e974fc8807bb927aafa602639b0
Xumar Nabiyeva <115024941+XumarN@users.noreply.github.com>
2022-12-22 01:22:57 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,37 @@ $('#owl-carousel').owlCarousel({
     nav: true,
     items: 2,
 })
+
+
+
+
+
+
+
+// var options = {
+//   valueNames: ['name', 'city']
+// };
+// var hackerList = new List('hacker-list', options);
+
+// // Assigning references to the filter buttons
+// var filterCityButton = document.getElementById("filter");
+// var removeFiltersButton = document.getElementById("removeFilters");
+
+// // When the filter button is clicked. The list is filtered by calling the filter function of the list object and passing in a function that accepts the list items.
+// filterCityButton.addEventListener("click", function() {
+//   hackerList.filter(function(item) {
+//     if (item.values().city == "Edmonton") {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }); // Only items that have Edmonton as the city.
+// });
+
+// // When the remove filter button is clicked, the filters are removed by calling the filter function once again with no parameters.
+// removeFiltersButton.addEventListener("click", function() {
+//   hackerList.filter();
+// });
+  
+
+
